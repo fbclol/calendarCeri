09a0d651174c560a728292dcdded0c9fe0fadbcd
Administrator <boue.franck@orange.fr>
2018-09-10 11:39:06 +0200

    debut sicom

--- index.php
+++ index.php
@@ -59,6 +59,9 @@ $jEvents = BuilderCalendar::createCalendar();
                         <option value="m2-alt-ingedoc">M2 ingedoc alternant</option>
                         <option value="m2-cla-ingedoc">M2 ingedoc classique</option>
                     </optgroup>
+                    <optgroup label="M2 - SICOM">
+                        <option value="test">test</option>
+                    </optgroup>
                 </select>
             </form>
         </div>
